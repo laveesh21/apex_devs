b8eb472edbe1d09c751230897230d16f7b2d6ce1
laveesh21 <laveeshtomar.royalking@gmail.com>
2024-03-28 23:54:38 +0530

    update cors middleware

--- server/server.js
+++ server/server.js
@@ -9,10 +9,13 @@ import AuthenticatorRouter from "./routes/auth-router.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors({
+  origin: 'https://apexdevs-club.onrender.com/',  // Replace with your frontend's domain
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: ['Content-Type', 'Authorization']}));
 
 
 app.use(bodyParser.json());
-app.use(cors());
 app.use("/project", ProjectRouter);
 app.use('/user', AuthenticatorRouter)
 
